3ce16b86393551e98cc0bdc585d8ad16f5efc6db
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2024-06-16 01:07:28 +0100

    Time: 3 ms (43.72%), Space: 7.2 MB (80.60%) - LeetHub

---
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        if(k == 1) return 1;
+        --k;
+        
+        for(int i = 2; i<=n/2; ++i)
+            if(n % i == 0) {
+                --k;
+                if(k == 0)
+                    return i;
+            }
+
+        return k==1 ? n : -1;
+    }
+};
