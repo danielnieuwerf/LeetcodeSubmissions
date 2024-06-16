9f19fce63a51003729f053b7f41390870e1ac012
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2024-06-16 12:01:20 +0100

    Time: 9 ms (68.42%), Space: 14 MB (38.59%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int,int> mp;
         for(auto it = nums.begin(); it<nums.end(); ++it) {
             if(mp.find(target-*it) != mp.end())
-                return {(int)distance(nums.begin(), it), mp[target-*it]};
+                return {static_cast<int>(distance(nums.begin(), it)), mp[target-*it]};
             mp[*it] = distance(nums.begin(), it);
         }
         return {-1, -1};
