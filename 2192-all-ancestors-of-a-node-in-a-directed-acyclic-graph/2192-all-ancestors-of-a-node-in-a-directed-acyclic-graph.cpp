2785137cf8178a06fe5c9c61ba38a9796c66e7d3
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2024-06-29 14:14:32 +0100

    Time: 397 ms (20.09%), Space: 183.1 MB (9.55%) - LeetHub

---
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    vector<vector<int>> dp;
+    vector<bool> computed;
+    unordered_map<int, vector<int>> mp;
+    
+public:
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+        dp = vector<vector<int>>(n, vector<int>(1, -1));
+        computed = vector<bool>(n, false);
+        for(auto edge: edges)
+            mp[edge[1]].push_back(edge[0]);
+        
+        vector<vector<int>> ans = vector<vector<int>>(n, vector<int>());
+        for(int i = 0; i<n; ++i)
+            ans[i] = ancestors(i);
+        return ans;
+    }
+    
+    vector<int> ancestors(int i) {
+        if(computed[i]) return dp[i];
+        computed[i] = true;
+        
+        set<int> a;
+        for(auto edge: mp[i]) {
+            a.insert(edge);
+            for(auto anc: ancestors(edge))
+                a.insert(anc);
+        }
+        
+        vector<int> ans;
+        for(auto anc: a) {
+            ans.push_back(anc);
+        }
+    
+        return dp[i] = ans;
+    }    
+};
