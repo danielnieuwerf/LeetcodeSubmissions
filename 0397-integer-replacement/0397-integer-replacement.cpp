d9da273da4f1185e51fb2bba06a1a29959507f68
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2024-07-01 23:21:38 +0100

    Time: 0 ms (100.00%), Space: 10.6 MB (24.71%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -1,16 +1,16 @@
 class Solution {
-    unordered_map<int,long> mp;
+    unordered_map<int,int> mp;
     
 public:
     int integerReplacement(int n) {
-        return integerReplacement((long) n);
-    }
-    
-    long integerReplacement(long n) {
-        if(n == 1) return 0;
-        if(mp.find(n) != mp.end()) return mp[n];
-        if(n%2 == 0) return mp[n] = (1 + integerReplacement(n/2));
-        
-        return mp[n] = (1 + min(integerReplacement(n+1), integerReplacement(n-1)));
+        if(n == INT_MAX) 
+            return 32;
+        if(n == 1) 
+            return 0;
+        if(mp.find(n) != mp.end()) 
+            return mp[n];
+        if(n%2 == 0) 
+            return mp[n] = (1 + integerReplacement(n/2));
+        return mp[n] = (1 + min(integerReplacement(n+1), integerReplacement(n-1)));    
     }
 };
