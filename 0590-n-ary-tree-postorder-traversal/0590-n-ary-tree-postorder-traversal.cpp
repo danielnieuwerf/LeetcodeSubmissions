53ffbb550a2acca57126664f0a6f7a1708625573
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2024-08-26 10:08:54 +0100

    Time: 13 ms (64.15%), Space: 15.3 MB (21.73%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    vector<int> _ans;
+public:
+    vector<int> postorder(Node* root) {
+        rec(root);
+        return _ans;
+    }
+    
+    void rec(Node* root) {
+        if(!root) return;
+        for(auto node: root->children)
+            rec(node);
+        _ans.push_back(root->val);
+    }
+};
