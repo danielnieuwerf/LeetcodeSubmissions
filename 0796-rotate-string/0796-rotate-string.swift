2531c822dfca74bb9fdef0c54467e8482b0982c4
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2024-11-03 01:21:17 +0000

    Time: 0 ms (100.00%), Space: 17.9 MB (8.47%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.swift
+++ 0796-rotate-string/0796-rotate-string.swift
@@ -1,13 +1,6 @@
 class Solution {
     func rotateString(_ s: String, _ goal: String) -> Bool {
         guard s.count == goal.count else { return false }
-        var possible = false
-        (0..<s.count).forEach {
-            var i = s.index(s.endIndex, offsetBy: -$0)            
-            if goal == s.suffix(from: i) + s[..<i] {
-                possible = true
-            }
-        }
-        return possible
+        return (s + s).contains(goal)
     }
 }
