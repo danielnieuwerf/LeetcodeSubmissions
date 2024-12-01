53b722b25cfb41ac4b086f24d2566739117c22ae
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2024-12-01 23:02:41 +0000

    Time: 4 ms (16.15%), Space: 13.9 MB (15.57%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_map<int,int> mp;
+        for(auto num: arr) {
+            if(mp.find(num*2) != mp.end())
+                return true;
+            if(num%2 == 0 && mp.find(num/2) != mp.end())
+                return true;
+            ++mp[num];
+        }
+        return false;
+    }
+};
