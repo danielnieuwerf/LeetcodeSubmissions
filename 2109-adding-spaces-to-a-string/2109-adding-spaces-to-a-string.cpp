46ca80d887fe8a182839809e63b116ecca431004
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2024-12-03 21:19:45 +0000

    Time: 22 ms (38.89%), Space: 85.5 MB (20.04%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.cpp
@@ -3,8 +3,9 @@ public:
     string addSpaces(string s, vector<int>& spaces) {
         string ans;
         int j = 0;
+        int len = spaces.size();
         for(int i = 0; i< s.size(); ++i) {
-            if(j<spaces.size() && spaces[j] == i) {
+            if(j<len && spaces[j] == i) {
                 ++j;
                 ans += " ";
             }
