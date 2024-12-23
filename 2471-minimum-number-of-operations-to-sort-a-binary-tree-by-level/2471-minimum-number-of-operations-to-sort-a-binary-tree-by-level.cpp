89090704fa34c1f745733e6be5f2a8118ff505e9
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2024-12-23 23:05:39 +0000

    Time: 383 ms (14.07%), Space: 247.4 MB (5.27%) - LeetHub

---
+++ 2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2471-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int ans = 0;
+public:
+    int minimumOperations(TreeNode* root) {
+        rec({root});
+        return ans;
+    }
+    
+    void rec(vector<TreeNode*> v) {
+        if(v.empty()) return;
+        vector<TreeNode*> nextLevel;
+        vector<int> values;
+        for(auto node: v) {
+            if(node->left)
+                nextLevel.push_back(node->left);
+            if(node->right)
+                nextLevel.push_back(node->right);
+            values.push_back(node->val);
+        }
+        ans += getMinSwapsNeededForSorting(values);
+        rec(nextLevel);
+    }
+    
+    int getMinSwapsNeededForSorting(vector<int>& values) {
+        int swaps = 0;
+        vector<int> target = values;
+        sort(target.begin(), target.end());
+
+        unordered_map<int, int> pos;
+        for (int i = 0; i < values.size(); i++) {
+            pos[values[i]] = i;
+        }
+
+        for (int i = 0; i < values.size(); ++i) {
+            if (values[i] != target[i]) {
+                ++swaps;
+                int curPos = pos[target[i]];
+                pos[values[i]] = curPos;
+                swap(values[curPos], values[i]);
+            }
+        }
+        return swaps;
+    }
+};
