de8f23634102df8e82e0af2a5040ceef95eff79e
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2025-01-04 15:33:28 +0000

    Time: 128 ms (91.11%), Space: 15.9 MB (56.14%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -4,13 +4,13 @@ public:
         int ans = 0;
         for(char c = 'a'; c<='z'; ++c) {
             size_t pos1 = s.find_first_of(c);
+            if(pos1 == string::npos) continue;
             size_t pos2 = s.find_last_of(c);
-            if(pos1 != string::npos && pos2 != string::npos && pos1 != pos2) {
-                unordered_map<char,int> mp;
-                for(auto i = pos1 +1; i < pos2; ++i)
-                    ++mp[s[i]];
-                ans += mp.size();
-            }
+            if(pos2 == string::npos || pos2 == pos1) continue;
+            unordered_map<char,int> mp;
+            for(auto i = pos1 +1; i < pos2; ++i)
+                ++mp[s[i]];
+            ans += mp.size();
         }
         return ans;
     }
