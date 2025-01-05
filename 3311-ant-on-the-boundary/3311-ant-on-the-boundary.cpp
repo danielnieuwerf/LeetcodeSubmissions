36907f3b5fd5383b6470a92c141b50114f2eee55
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2025-01-05 16:52:46 +0000

    Time: 7 ms (27.93%), Space: 23.5 MB (11.87%) - LeetHub

--- 3311-ant-on-the-boundary/3311-ant-on-the-boundary.cpp
+++ 3311-ant-on-the-boundary/3311-ant-on-the-boundary.cpp
@@ -3,7 +3,7 @@ public:
     int returnToBoundaryCount(vector<int>& nums) {
         int ans = 0;
         for(int i = 1; i<nums.size(); ++i) {
-            nums[i] = nums[i-1] + nums[i];
+            nums[i] += nums[i-1];
             if(nums[i] == 0)
                 ++ans;
         }
