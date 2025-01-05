1db445e9d4d7c10fa1214b8b1df99c0a7ce959ba
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2025-01-05 12:42:00 +0000

    Time: 36 ms (43.67%), Space: 108.3 MB (27.49%) - LeetHub

---
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        int n = s.size();
+        vector<int> prefixSum(n+1, 0);
+        for(auto shift: shifts) {
+            int start = shift[0], end = shift[1], direction = shift[2] == 1 ? 1 : -1;
+            prefixSum[start] += direction;
+            prefixSum[end+1] -= direction;
+        }
+
+        int currentShift = 0;        
+        for(int i = 0; i<n; ++i) {
+            currentShift += prefixSum[i];
+            int shift = (s[i]-'a' + currentShift)%26;
+            if(shift < 0)
+                shift += 26;
+            s[i] = 'a' + shift;
+        }
+        return s;
+    }
+};
