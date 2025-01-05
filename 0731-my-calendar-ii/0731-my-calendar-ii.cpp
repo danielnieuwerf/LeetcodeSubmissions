755166c18e193228159777c4a85283707d1beb73
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2025-01-05 15:33:58 +0000

    Time: 73 ms (67.65%), Space: 44.3 MB (10.04%) - LeetHub

---
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -0,0 +1,34 @@
+class MyCalendarTwo {
+    map<int,int> bookings;
+public:
+    MyCalendarTwo() {
+
+    }
+    
+    bool book(int start, int end) {
+        ++bookings[start];
+        --bookings[end];
+        int overlappingBookings = 0;
+        for(auto& p: bookings) {
+            overlappingBookings += p.second;
+            if (overlappingBookings >= 3) {
+                --bookings[start];
+                ++bookings[end];                
+                if (bookings[start] == 0) {bookings.erase(start);}
+                if (bookings[end] == 0) {bookings.erase(end);}
+                return false;
+            }
+
+            if(p.first > end){
+                break;
+            }
+        }
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(startTime,endTime);
+ */
