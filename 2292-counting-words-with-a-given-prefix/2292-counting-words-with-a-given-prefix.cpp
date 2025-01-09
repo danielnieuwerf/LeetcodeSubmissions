1875f825c875ff0c09a33dcbe1221e484f10462b
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2025-01-09 21:16:33 +0000

    Time: 1 ms (33.67%), Space: 12.9 MB (95.17%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
@@ -3,7 +3,7 @@ public:
     int prefixCount(vector<string>& words, string pref) {
         int ans = 0;
         for(auto& word: words)
-            if(auto pos = word.find(pref); pos != string::npos && pos == 0)
+            if(auto pos = word.find(pref); pos == 0)
                 ++ans;
         return ans;
     }
