14f1393d6c45fad332923b1b8fbb99245ce38d55
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2025-01-13 22:41:54 +0000

    Time: 9 ms (81.54%), Space: 32.2 MB (58.08%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int minimumLength(string s) {
         vector<int> frequencies(26,0);
-        for(auto c: s) {
+        for(char c: s) {
             ++frequencies[c-'a'];
         }
         int ans = 0;
-        for(auto freq: frequencies)
+        for(int freq: frequencies)
         {
             if(freq == 0) continue;
             if(freq % 2 == 1)
