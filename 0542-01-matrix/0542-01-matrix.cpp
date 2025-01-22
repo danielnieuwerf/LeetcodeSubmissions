634cf957821f931ee0043a98f5d7df3a37c106e4
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2025-01-22 14:13:13 +0000

    Time: 11 ms (86.58%), Space: 38.9 MB (24.8%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        vector<vector<int>> ans(n, vector<int>(m,-1));
+        vector<int> current;
+        for(int i = 0; i<n; ++i)
+            for(int j = 0; j<m; ++j) {
+                if(mat[i][j] == 0)
+                {
+                    current.push_back(i*m + j);
+                    ans[i][j] = 0;
+                }
+            }
+        if(current.empty())
+            current.push_back(0);
+
+        int currentLevel = 0;
+        while(!current.empty()) {
+            vector<int> nextLevel;
+            for(auto index: current) {
+                int i = index/m, j = index%m;
+                if(ans[i][j] > 0) continue;
+                ans[i][j] = currentLevel;
+                if(i<n-1 && ans[i+1][j] == -1)
+                    nextLevel.push_back((i+1)*m+j);
+                if(i>0 && ans[i-1][j] == -1)
+                    nextLevel.push_back((i-1)*m+j);
+                if(j<m-1 && ans[i][j+1] == -1)
+                    nextLevel.push_back(i*m+j+1);
+                if(j>0 && ans[i][j-1] == -1)
+                    nextLevel.push_back(i*m+j-1);
+            }
+            ++currentLevel;
+            current = nextLevel;
+        }
+
+        return ans;
+    }
+};
