68bd213108a630c255d3bdfdbc1b68d2998a8a65
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2025-01-22 21:41:34 +0000

    Time: 0 ms (100%), Space: 22.5 MB (44.84%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.cpp
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n = citations.size();
-        int low = 0, high = n;
+        int low = 0, high = n - 1;
         int best = 0;
         while(low<=high) {
             int mid = (low+high)/2;
-            int citationsAtLeastMid = 0;
-            for(int count: citations)
-                if(count >= mid)
-                    citationsAtLeastMid++;
-            if(citationsAtLeastMid >= mid) {
-                best = mid;
+            if(citations[mid] == n - mid) {
+                return citations[mid];
+            } else if(citations[mid] < n - mid) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
-        return best;
+        return n - low;
     }
 };
