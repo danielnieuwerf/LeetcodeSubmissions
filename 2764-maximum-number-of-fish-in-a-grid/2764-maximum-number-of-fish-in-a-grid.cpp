59f543175fd44bb2cf9ea1824b73456e842703c3
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2025-01-28 21:24:13 +0000

    Time: 14 ms (42.31%), Space: 101.4 MB (29.71%) - LeetHub

---
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    int n = 0;
+    int m = 0;
+    set<long> visited;
+public:
+    int findMaxFish(vector<vector<int>>& grid) {
+        n = grid.size();
+        m = grid[0].size();
+        int best = 0;
+        for(int i = 0; i<n; ++i) {
+            for(int j = 0; j<m; ++j) {
+                if(grid[i][j] == 0)
+                    continue;
+                best = max(best, dfs(grid, i, j));
+            }
+        }
+        return best;
+    }
+
+    int dfs(vector<vector<int>>& grid, int i, int j) {
+        if(i<0 || i>= n || j<0 || j>=m || grid[i][j] == 0 || visited.contains(i << 16 | j)) return 0;
+        visited.insert(i<<16 | j);
+        return grid[i][j] + dfs(grid, i, j+1) + dfs(grid, i, j-1) + dfs(grid, i-1, j) + dfs(grid, i+1, j);
+    }    
+};
