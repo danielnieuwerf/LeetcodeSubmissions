14fb27008eae1ef23dbd1b02bf559a960fe58b25
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2025-03-01 12:19:12 +0000

    Time: 0 ms (100%), Space: 13.1 MB (87.59%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        for(int i = 0; i<nums.size()-1; ++i) {
+            if(nums[i] == nums[i+1]) {
+                nums[i] *= 2;
+                nums[i+1] = 0;
+            }
+
+        }
+
+        int zeroes = 0;
+        for(auto num: nums)
+            if(num == 0)
+                ++zeroes;
+
+        vector<int> ans;
+        for(auto num: nums)
+            if(num != 0)
+                ans.push_back(num);
+        while(zeroes--) {
+            ans.push_back(0);
+        }
+        return ans;
+    }
+};
