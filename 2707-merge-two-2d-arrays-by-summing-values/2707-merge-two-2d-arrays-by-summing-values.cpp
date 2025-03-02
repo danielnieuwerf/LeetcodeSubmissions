9046c4cb8cf332d336e84fb7154e0e3660589702
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2025-03-02 23:32:56 +0000

    Time: 5 ms (10.48%), Space: 15.3 MB (22.15%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        map<int,int> mp{};
+        map<int,int> mp;
         for(auto n: nums1)
         {
             mp[n[0]] += n[1];
