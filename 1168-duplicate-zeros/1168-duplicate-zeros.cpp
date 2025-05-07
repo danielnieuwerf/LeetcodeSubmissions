1131e04b7bacc66de71df057de630995d6cc583f
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2025-05-07 22:39:38 +0100

    Time: 0 ms (100%), Space: 13.9 MB (64.45%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.cpp
+++ 1168-duplicate-zeros/1168-duplicate-zeros.cpp
@@ -5,13 +5,10 @@ public:
         vector<int> ans(n, 0);   
         int j = 0;
         for(int i = 0; j<n; ++i, ++j) {
-            if(arr[i] == 0) {
-                ans[j] = 0;
-                if(j<n-1)
-                    ans[j+1] = 0;
+            ans[j] = arr[i];
+            if(arr[i] == 0 && j < n-1) {
+                ans[j+1] = 0;
                 ++j;
-            } else {
-                ans[j] = arr[i];
             }
         }
         arr = ans;
