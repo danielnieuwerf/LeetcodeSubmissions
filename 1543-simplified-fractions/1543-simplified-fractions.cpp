602e78b50f3fa7935f29ab609e5e362d9a3e7a44
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2025-05-31 23:02:41 +0100

    Time: 24 ms (72.88%), Space: 26.1 MB (64.97%) - LeetHub

---
+++ 1543-simplified-fractions/1543-simplified-fractions.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> simplifiedFractions(int n) {
+        vector<string> ans;
+        if(n == 1) {
+            return {};
+        }
+        for(int i = 1; i<=n; ++i) {
+            for(int j = i+1; j<=n; ++j) {
+                if(__gcd(i,j) != 1)
+                    continue;
+                ans.push_back(to_string(i)+"/"+to_string(j));
+            }
+        }
+        return ans;
+    }
+};
