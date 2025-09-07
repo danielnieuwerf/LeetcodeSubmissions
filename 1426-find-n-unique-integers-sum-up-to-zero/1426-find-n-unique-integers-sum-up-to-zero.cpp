1fd258fa2b165eda7100ba3c7b82805903b7d2f6
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2025-09-07 09:53:43 +0100

    Time: 0 ms (100%), Space: 9.7 MB (13.58%) - LeetHub

---
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> ans;
+        if(n%2 == 1) {
+            ans.push_back(0);
+            --n;
+        }
+        if(n%2 == 0) {
+            int curr = 1;
+            while(n) {
+                ans.push_back(curr);
+                curr *= -1;
+                --n;
+                if(curr > 0) {
+                    ++curr;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
