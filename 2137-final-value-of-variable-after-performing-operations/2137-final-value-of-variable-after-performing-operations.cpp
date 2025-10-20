cb7f10ca34426644d6cf059ba73f1560266016ad
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2025-10-20 18:38:45 +0100

    Time: 0 ms (100%), Space: 17.6 MB (42.92%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for(auto& op: operations) {
+            op.contains("-") ? --x : ++x;
+        }
+        return x;
+    }
+};
