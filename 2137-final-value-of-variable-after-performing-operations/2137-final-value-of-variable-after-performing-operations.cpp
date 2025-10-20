f311d621b2ba0fcc8b4e1fa679988e11c69d4974
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2025-10-20 18:39:25 +0100

    Time: 0 ms (100%), Space: 17.7 MB (10.37%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cpp
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
         int x = 0;
-        for(auto& op: operations) {
+        for(auto op: operations) {
             op.contains("-") ? --x : ++x;
         }
         return x;
