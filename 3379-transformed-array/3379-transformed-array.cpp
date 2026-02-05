8460a5fcc5d12644394529a0ec5a665893af8bab
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2026-02-05 12:56:03 +0000

    Time: 13 ms (5.85%), Space: 25.8 MB (8.08%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.cpp
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     vector<int> constructTransformedArray(vector<int>& nums) {
         int n = nums.size();
-        if(n==1) {
-            return nums;
-        }
         vector<int> ans(n, -1);
         for(int i = 0; i<n; ++i) {
-            if (nums[i] != 0) {
-                ans[i] = nums[mymod(i + nums[i], n)];
-            } else {
+            if (nums[i] == 0) {
                 ans[i] = nums[i];
+                continue;
             }
+            ans[i] = nums[mod(i + nums[i], n)];
         }
         return ans;
     }
 
-    int mymod(int x, int y) {
+    int mod(int x, int y) {
         return ((x % y) + y) % y;
     }
 };
