21e7a34b618754be0da6d036afc033c0a164598d
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2026-03-09 23:06:59 +0000

    Time: 4 ms (64.55%), Space: 13.3 MB (79.4%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans(2*n, -1);
+        for(int i = 0; i<n; ++i) {
+            ans[2*i] = nums[i];
+        }
+        for(int i = n; i<2*n; ++i) {
+            ans[2*(i - n) +1] = nums[i];
+        }
+        return ans;
+    }
+};
