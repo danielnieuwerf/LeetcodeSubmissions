143232ba58338b03cc7835837d6d753506a98ed3
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2026-04-18 14:24:24 +0100

    Time: 2 ms (20.08%), Space: 9.2 MB (77.05%) - LeetHub

--- 0970-powerful-integers/0970-powerful-integers.cpp
+++ 0970-powerful-integers/0970-powerful-integers.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     vector<int> powerfulIntegers(int x, int y, int bound) {
         set<int> s;
-        for(int i=0;;++i) {
+        for(int i=0;; ++i) {
             auto X = pow(x,i);
-            for(int j = 0; ; ++j) {
+            for(int j = 0;; ++j) {
                 int curr = X+pow(y,j); 
-                if(curr <= bound) {
-                    s.insert(curr);
-                } else {
+                if (curr > bound)
                     break;
-                }
+                s.insert(curr);
                 if(y == 1)
                     break;
             }
