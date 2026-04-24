53a513dacd7ad1a4c4d537d65e9e80e8a4a02b28
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2026-04-25 00:22:12 +0100

    Time: 12 ms (0.58%), Space: 13.7 MB (6.09%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int furthestDistanceFromOrigin(string moves) {
-        set<int> pos = {0};
+        unordered_set<int> pos = {0};
         int n = moves.size();
         for(int i = 0; i<n; ++i) {
-            set<int> newPos;
+            unordered_set<int> newPos;
             for(auto val: pos) {
                 if(moves[i] == 'L') {
                     newPos.insert(val-1);
