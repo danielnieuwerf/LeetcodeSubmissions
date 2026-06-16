2f32cd5f4a4c092ac8cd974cd97457d9bfbf264e
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2026-06-16 22:20:54 +0100

    Time: 4 ms (63.88%), Space: 39.8 MB (69.58%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string ans = "";
+        for(auto c: s) {
+            if(c == '*') {
+                if(!ans.empty()) {
+                    ans = ans.substr(0, ans.size()-1);
+                }
+            } else if(c == '#') {
+                ans = ans + ans;
+            } else if(c == '%') {
+                reverse(ans.begin(), ans.end());
+            } else {
+                ans += c;
+            }
+        }
+        return ans;
+    }
+};
