355b29b8e1963f9268f28f1c2f855ce9b40be388
Daniel Nieuwerf <60330137+danielnieuwerf@users.noreply.github.com>
2026-07-12 10:52:54 +0100

    Time: 68 ms (21.57%), Space: 41 MB (80.5%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> a = arr;
+
+        sort(arr.begin(), arr.end());
+        unordered_map<int,int> mp{};
+
+        int rank = 1;
+        for(auto& num: arr)
+            if(mp.find(num) == mp.end())
+                mp[num] = rank++;
+
+
+        vector<int> ans;
+        for(int num: a)
+            ans.push_back(mp[num]);
+
+        return ans;
+    }
+};
